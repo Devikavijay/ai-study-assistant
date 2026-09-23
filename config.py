83e2dd4213072588337448564d7e11e16c814a1c
Devika Vijay <devikavijay99@gmai.com>
2026-09-23 19:01:04 +0530

    Add Streamlit secrets support

--- config.py
+++ config.py
@@ -1,10 +1,19 @@
 import os
+import streamlit as st
 from dotenv import load_dotenv
 from google import genai
 
 load_dotenv()
 
 api_key = os.getenv("GEMINI_API_KEY")
+
+if not api_key:
+    try:
+        api_key = st.secrets["GEMINI_API_KEY"]
+
+    except (KeyError, FileNotFoundError):
+        api_key = None
+
 
 if not api_key:
     raise ValueError("GEMINI_API_KEY was not found.")
